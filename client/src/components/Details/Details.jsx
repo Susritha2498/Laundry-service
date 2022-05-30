686819a149fa3616b7ca04a1d5204945feb494e0
Susritha2498 <susritha.balusu@gmail.com>
2022-05-30 11:16:24 +0530

    Navbar Details Pastorders sidebar footer

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import "./Details.css"
+import {Images} from '../../constants/index'
 
 const Details = () => {
   return (
-    <div>
-      <p>About us</p>
-      <p>Contact details</p>
-      <p>Careers </p>
-      <p>Social media links</p>
+    <div className='app-details'>
+      <div className='app-offers'>
+        <h2>Now Refer & Earn ₹500 for every referral*</h2>
+        <p>* Terms and conditions will be applied</p>
+      </div>
+
+      <div className='app-sign-footer'>
+        <div id='app-about'>
+          <h3>ABOUT US</h3>
+          <p>Doorstep Wash & Dry Clean Service</p>
+        </div>
+
+        <div id='app-home'>
+          <h4>Home</h4>
+          <p>Sign In</p>
+          <p>Register</p>
+        </div>
+
+        <h3>Pricing</h3>
+
+        <div id='app-career'>
+          <h4>Career</h4>
+          <p>Blogs</p>
+          <p>Create</p>
+        </div>
+
+        <h3>Contact</h3>
+
+        <div id='app-socialmedia'>
+          <h3>SOCIAL MEDIA</h3>
+          <div className='app-media-icons'>
+            <img src={Images.facebook} alt="facebook" />
+            <img src={Images.instagram} alt="instagram" />
+            <img src={Images.linkedin} alt="linkedin" />
+          </div>
+        </div>
+
+        </div>
     </div>
   )
 }
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Images } from '../../constants'
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='app-sidebar'>
+      <div className='app-side-icon1'><img src={Images.home} alt="Home" id='side1'/></div>
+      <div className='app-side-icon1'><img src={Images.add} alt="addnew" id='side2'/></div>
+      <div className='app-side-icon2'><img src={Images.list} alt="orderslist" id='side3'/></div>
+    </div>
   )
 }
 
